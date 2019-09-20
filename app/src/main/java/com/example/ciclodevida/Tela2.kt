1fd1f13e6644b19efe3c0dd35637685c2ad0c2cc
raylison nunes dos santos <raylison100@gmail.com>
2019-09-20 07:56:03 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ciclodevida/Tela2.kt
@@ -0,0 +1,12 @@
+package com.example.ciclodevida
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Tela2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
